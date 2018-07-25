0e3a5b25ccb5b87f15d614c581dea0e97116d406
Victor tran <victor.n.tran.social@outlook.com>
2018-07-25 01:59:09 -0400

    Retrieved lost configuration for donation form and added missing styles for donation form

--- src/containers/StripeDonationForm/StripeDonationForm.js
+++ src/containers/StripeDonationForm/StripeDonationForm.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Form from "../../components/Form/Form";
 
 import { cardData } from "../../config";
+import styles from "./StripeDonationForm.css";
 
 class StripeDonationForm extends Component {
   constructor(props) {
@@ -9,27 +10,55 @@ class StripeDonationForm extends Component {
     this.state = {
       cardName: "",
       cardNumber: "",
-      cvv: ""
+      cvv: "",
+      expiration: "",
+      amount: ""
     };
   }
 
   onChangeHander = event => {
     const { name, value } = event.target;
-    this.setState({
-      ...this.state,
-      [name]: value
+    this.setState(
+      {
+        ...this.state,
+        [name]: value
+      },
+      () => {
+        this.formatDate();
+      }
+    );
+  };
+
+  formatDate = () => {
+    this.setState(prevState => {
+      let joinedExpiration = prevState.expiration;
+      if (prevState.expiration.includes("/")) {
+        joinedExpiration = prevState.expiration.split("/").join("");
+      }
+      const newExpiration = joinedExpiration;
+      console.log(joinedExpiration.length > 2);
+      if (joinedExpiration.length > 2) {
+        const month = joinedExpiration.substr(0, 2);
+        const year = joinedExpiration.substr(2, 4);
+        const newExpiration = `${month}/${year}`;
+        return { expiration: newExpiration };
+      }
+      return {
+        expiration: newExpiration
+      };
     });
   };
 
   render() {
-    console.log("Stripe Form: ", this.state);
     return (
-      <Form
-        name="Donations"
-        data={cardData}
-        onChange={this.onChangeHander}
-        state={this.state}
-      />
+      <div className={styles.StripeDonationForm}>
+        <Form
+          name="Donations"
+          data={cardData}
+          onChange={this.onChangeHander}
+          state={this.state}
+        />
+      </div>
     );
   }
 }
